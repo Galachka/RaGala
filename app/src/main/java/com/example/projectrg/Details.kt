f1f0305da1e84c0a3fdd90ae28557c0a718d6c16
Galachka <esbolov.galymzhan2003@gmail.com>
2022-10-29 20:25:08 +0600

    finish of details

--- app/src/main/java/com/example/projectrg/Details.kt
+++ app/src/main/java/com/example/projectrg/Details.kt
@@ -1,10 +1,14 @@
 package com.example.projectrg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_details.*
 
 class Details : AppCompatActivity() {
+    private lateinit var obj:GeneralData
+    private var planetImg:Int?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -12,5 +16,24 @@ class Details : AppCompatActivity() {
         window.decorView.apply { systemUiVisibility =
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         }
+        obj= intent.getParcelableExtra("planet")!!
+        planetImg=intent.getIntExtra("planetImage",-1)
+        setData(obj, planetImg!!)
+
+        button_info.setOnClickListener{
+            val intent= Intent(this,finalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun setData(obj:GeneralData,planetImg:Int)
+    {
+        title_info.text=obj.title
+        distance_info.text=obj.distance+"m km"
+        gravity_info.text=obj.gravity+" m/ss"
+        overview_info.text=obj.overview
+        galaxy_info.text=obj.galaxy
+        planet_img_info.setImageResource(planetImg)
+
+
     }
 }
