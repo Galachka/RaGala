7c4894717744d06e15f3a671bc1a6d1ada1baeed
Galachka <esbolov.galymzhan2003@gmail.com>
2022-10-29 18:57:02 +0600

    add objects in class myiewholder

--- app/src/main/java/com/example/projectrg/Adapter.kt
+++ app/src/main/java/com/example/projectrg/Adapter.kt
@@ -4,10 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view.view.*
 
-class Adapter: RecyclerView.Adapter<Adapter.myViewHolder>() {
+class Adapter( val planet:List<GeneralData>): RecyclerView.Adapter<Adapter.myViewHolder>() {
     class myViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val title = view.title
+        val planetimg = view.planet_img
+        val galaxy = view.galaxy
+        val distance = view.distance
+        val gravity = view.gravity
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
@@ -21,6 +26,6 @@ class Adapter: RecyclerView.Adapter<Adapter.myViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return planet.size
     }
 }
