c5f922f7030727e3cc181a096134d71e423358d2
Galachka <esbolov.galymzhan2003@gmail.com>
2022-10-29 20:21:18 +0600

    add fullscreen to details

--- app/src/main/java/com/example/projectrg/Details.kt
+++ app/src/main/java/com/example/projectrg/Details.kt
@@ -2,10 +2,15 @@ package com.example.projectrg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Details : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        window.decorView.apply { systemUiVisibility =
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
