5ae91d7ada9146026d43ea639d7425fd405d858a
Galachka <esbolov.galymzhan2003@gmail.com>
2022-10-31 12:32:33 +0600

    testing drawer

--- app/src/main/java/com/example/projectrg/MainActivity.kt
+++ app/src/main/java/com/example/projectrg/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.projectrg
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
